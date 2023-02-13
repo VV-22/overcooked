bbf4b63f665f85e81d30a5429152ee9b8dd5db2e
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-02-13 19:57:51 +0530

    New inputSys and animations
    WASD, Arrow keys and some basic idle and movement animations.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float moveSpeed;
+    [SerializeField]private GameInput gameInput;
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0f,0f);
-        if(Input.GetKey(KeyCode.W))
-            inputVector.y=1f;
-        else if (Input.GetKey(KeyCode.S))
-            inputVector.y=-1f;
-        if(Input.GetKey(KeyCode.A))
-            inputVector.x = -1f;
-        else if(Input.GetKey(KeyCode.D))
-            inputVector.x = 1f;
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         //Debug.Log(inputVector);
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = (moveDir != Vector3.zero);
