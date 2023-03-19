afc9e697666f8a7516ca33cafc158f1790a42a7d
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-03-19 19:34:24 +0530

    Pickup and drop objects
    Created prefab variants and set values for cabbage, tomato, bread and meat

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,7 +9,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if(!HasKitchenObject())
+        {
+            //counter is empty
+            if(player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //player not carrying anything
+            }
+        }
+        else
+        {
+            //there is a kitchenObject
+            if(player.HasKitchenObject())
+            {
+                //player is carrying something
+            }
+            else
+            {
+                //player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
 }
