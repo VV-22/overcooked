b606e5f4d4092c16c06d6f9c902b994343563f9a
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-04-11 09:02:31 +0530

    plate complete visual
    full visual of whatever is on the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
 
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
     private List<KitchenObjectSO> kitchenObjectSOList;
     [SerializeField]private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -15,13 +21,21 @@ public class PlateKitchenObject : KitchenObject
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
         if(!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            //not a valid ingredient
             return false;
+        }
         if(!kitchenObjectSOList.Contains(kitchenObjectSO))
         {
+            //valid ingredient not present in the current plate
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{kitchenObjectSO = kitchenObjectSO});
             return true;
         }
         else
+        {
+            //valid ingredient but already present in the plate
             return false;
+        }
     }
 }
