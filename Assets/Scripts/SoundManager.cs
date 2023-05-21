98bbcc30a22b251c4cb7fd6c0e689e2218920bc6
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-05-21 19:41:52 +0530

    Sound options
    Sound options with menu screen

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,10 +7,14 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance {get; private set;}
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+    private const String PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+    private float volume = 1f;
 
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME , 1f);
     }
     private void Start()
     {
@@ -57,9 +61,9 @@ public class SoundManager : MonoBehaviour
         PlaySound(audioClipRefsSO.deliverySuccess , deliveryCounter.transform.position);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
 
     }
     private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
@@ -69,5 +73,21 @@ public class SoundManager : MonoBehaviour
     public void PlayFootstepSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+
+    public void changeVolume()
+    {
+        volume += 0.1f;
+        if(volume > 1f)
+            volume = 0f;
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
